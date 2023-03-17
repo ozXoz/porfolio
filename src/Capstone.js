e4f90747f0c68b215bb875b69c4c203ec4df2ebb
ozXoz <honourkorkmaz@gmail.com>
2023-03-16 20:59:47 -0400

    it has been updated

--- src/Capstone.js
+++ src/Capstone.js
@@ -1,9 +1,109 @@
 
-
+import './Capstone.css'
 export const Capstone = () => {
+
+    const styles = {
+        container: {
+            padding: '150px',
+            fontSize: '19px',
+            lineHeight: '1.5',
+            textAlign: 'center',
+            animation: 'fadein 2s ease-in-out'
+
+        },
+        heading: {
+            fontSize: '36px',
+            marginBottom: '30px',
+        },
+    };
     return (
-        <div>
-            Capstone
+        <div style={styles.container}>
+       <div>
+           <h1 className={"box"}>•	Project Summary</h1>
+           <p>Web app that has features to promote collaboration between. 3 main features would be, video group chat, resources for learning, person blog for showing personal achievements and to find friends that like similar things, Whiteboard that helps the session with tutor by giving them tools for communication of concepts.
+
+               Promotional we will give free admin tools to the tutor, paid for by the students funding, students will also pay for the entire cloud. The student pays us and we give commission to the tutor. The tutor will receive recommendations for student blog profile matchmaking.
+           </p>
+       </div>
+
+            <div>
+                <h1 className={"box"}>•	Project Vision</h1>
+                <p1>
+                    In this section, we will discuss the business opportunity that our Online Tutoring web platform (OTweb) presents, the problem statement that our platform aims to address, our product position statement, and our SWOT analysis.
+
+                    Many students struggle to find the right tutor who can help them with their studies, and they often have to pay exorbitant fees for tutoring services. This can lead to poor academic performance and a lack of confidence in their abilities. Additionally, there is a need for a platform that can provide a safe and secure environment for students and tutors to connect, collaborate and learn together. Our platform aims to address these issues by providing affordable and effective tutoring services, while ensuring a secure and trustworthy online environment for all users.
+
+                    Our Online Tutoring web platform, OTweb, is an online platform that connects students with expert tutors for personalized one-on-one sessions. Our platform offers a wide range of features, including an online library, whiteboards for video chats, digital resources, an online collaboration solution, and a personal blog for students and tutors. Our platform offers high-quality tutoring services at affordable prices and provides a secure and trustworthy online environment for all users.
+
+                    Strengths: ● Our platform offers personalized one-on-one sessions with expert tutors. ● Our platform provides a wide range of features that aid the education process. ● Our platform provides a secure and trustworthy online environment. ● Our platform offers affordable prices for tutoring services.
+
+                    Weaknesses: ● Our platform may not have a large customer base initially, as it is a new platform. ● Our platform may face stiff competition from established players in the online tutoring market.
+                    Opportunities: ● There is a growing demand for online tutoring services. ● Our platform can expand globally to reach a larger customer base.
+
+                </p1>
+            </div>
+            
+
+            <div>
+                <h1 className={"box"}>•	Project Plan</h1>
+                <p>Instant help for students who need help, with tutors available online: tutor connection who is online, a menu</p>
+                <p>Online Library, where students could read pdf. ex) helpful techniques for students and tutor, How to build good habits for students </p>
+                <p>Whiteboards to facilitate video chat: drawing tools ex) shapes, pencil, photos(textbook page img)</p>
+                <p>Personal User Blog: Blog post (text),post profile pic, show website achievements,
+                    find others with similar courses.
+                </p>
+                <p>Digital Library for students to access their notes</p>
+                <p>Online collaboration solution</p>
+                <p>User authentication with google, facebook, github and/or linkedin</p>
+                <p>Payment processing page with support for Google Pay, Interact Visa and PayPal</p>
+                <p>Chat support feature for tech and billing issues</p>
+                <p>Contact us page</p>
+                <p>Online Tutoring webpage</p>
+                <p>Login functionality</p>
+                <p>Registration functionality</p>
+                <p>Payment functionality</p>
+                <p>Video Chat functionality</p>
+                <p>Tutor list and selection page</p>
+                <p>Tutor profile</p>
+                <p>Tutor notifications</p>
+                <p>Whiteboard for tutors</p>
+                <p>User profile</p>
+                <p>Account deactivation process</p>
+                <p>Tutor payment receiving system</p>
+                <p>Tutor rating system</p>
+                <p>Digital Library for students</p>
+                <p>Android app</p>
+                <p>iOS application</p>
+                <p>Administration panel </p>
+            </div>
+
+            <div>
+                <h1 className={"box"}>•	Requirements Analysis and Design </h1>
+                <p1>
+                    The purpose of this document is to present a detailed information of the Online Tutoring web. It will explain the purpose and features of the application, interfaces of the app, what the app will do, constraints under which it must operate and how the app will respond to external uses. This Document is intended for stakeholders and the developers of this app.
+                    This software will be Online Tutoring web for students who need help with their homework projects. This application will be designed to maximize the students productivity and education potential by providing tools to get help for online homework assignments as well as give tutors more flexibility to work on helping students with their homework. This software will facilitate communication with tutors and students through video chat, email and chat support. This software will also have admin page for administrators to modify this application as well.
+                </p1>
+            </div>
+
+
+
+            <div>
+                <h1 className={"box"}>•	System Implementation</h1>
+                <p>React We used React to build modern and snappy
+                application that cab work across all devices</p>
+                <p>JavaScript, We used JavaScript to help power
+                modern web application</p>
+                <p>Socket.io, Technology will be used for chat
+                application</p>
+                <p>O-auth, Used to authenticate the application</p>
+                <p>GraphQl executes queries using type system that is
+                defined for registered students quickly</p>
+                <p>CSS, Style and look for web page</p>
+                <p>Bootstrap, Design to make web page faster and
+                responsive to any device</p>
+            </div>
+
+
 
         </div>
     );
--- src/WorkExperience.js
+++ src/WorkExperience.js
@@ -1,6 +1,38 @@
+import React from "react";
+import './WorkExperience.css'
+
 export const WorkExperience = ()=>{
-    return(
-        <div>Work Exp</div>
-        
-    )
+
+    const styles = {
+        container: {
+            padding: '150px',
+            fontSize: '19px',
+            lineHeight: '1.5',
+            textAlign: 'center',
+
+        },
+        heading: {
+            fontSize: '36px',
+            marginBottom: '30px',
+        },
+    };
+    return (
+        <div style={styles.container}>
+            <h2 style={styles.heading}>Work Experience</h2>
+            <p>As a freelance developer, I have worked on a variety of projects using different programming languages and frameworks. Some of the technologies that I have experience with include:</p>
+
+            <p>PHP: I have developed multiple web applications using PHP, including an e-commerce website that included features such as product listings, shopping cart, and checkout process.</p>
+
+            <p>C#: I have built a customer service application using C# and ASP.NET, which included a GraphQL API for customers to submit support requests and customer service representatives to manage and respond to those requests.</p>
+            <p>Java: I have developed multiple Java-based applications, including a Spring Boot microservice that allowed users to manage their to-do lists. The microservice was deployed using Docker and Kubernetes.</p>
+            <p>Spring Boot MVC: I have worked on multiple projects using the Spring Boot MVC framework, including the customer service application mentioned above. The MVC architecture helped to keep the code organized and maintainable.</p>
+            <p>TypeScript: I have developed cross-platform mobile applications using TypeScript and React Native, including an app that allowed users to track their daily water intake and set reminders.</p>
+            <p>React: I have developed multiple web applications using React, including a responsive web application that allowed users to create, edit, and view articles and comments.</p>
+            <p>Node.js: I have created multiple RESTful APIs using Node.js and Express, including an API that allowed users to interact with a database of products. The API was secured with JSON Web Tokens (JWT) and implemented pagination, filtering, and sorting.</p>
+            <p>MongoDB: I have integrated MongoDB with multiple applications, including the Spring Boot microservice mentioned above. I designed the database schema to allow for efficient querying and indexing, which helped to ensure that the application was scalable.</p
+            >
+            <p>Overall, working with these technologies has allowed me to gain a broad range of experience and to develop a deep understanding of different programming paradigms. I am comfortable working on both frontend and backend development, and I am always eager to explore new technologies and learn new skills</p>
+
+        </div>
+    );
 }
